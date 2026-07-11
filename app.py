0e0b56a46f9d20a2a1f317e59949600d8971e4e4
elcadena715 <cadensamuel715@gmail.com>
2026-07-11 16:14:23 -0500

    Prueba de Api noticias

--- app.py
+++ app.py
@@ -84,6 +84,7 @@ if api_key_news:
         query_str = " OR ".join([a["symbol"] for a in activos_db])
         url = f"https://newsapi.org/v2/everything?q={query_str}&language=es,en&sortBy=publishedAt&pageSize=6&apiKey={api_key_news}"
         res = requests.get(url, timeout=3).json()
+        print(f"DEBUG API: Recibí {len(res.get('articles', []))} noticias nuevas de NewsAPI")
         if res.get("status") == "ok" and res.get("articles"):
             noticias_actuales = res["articles"]
     except Exception:
