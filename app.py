9fcbafadefb1a9ad7cfc486b8ec8002fc8972801
elcadena715 <cadensamuel715@gmail.com>
2026-07-12 17:27:27 -0500

    Cambios en el app.py

--- app.py
+++ app.py
@@ -182,7 +182,6 @@ with tab1:
                         st.session_state.selected_news = (noti, activo_rel, sid)
                         st.session_state.view = 'detail'
                         st.rerun()
-                    return
 
     # --- FICHA DE AUDITORÍA (DETALLE) ---
     elif st.session_state.view == 'detail':
@@ -191,7 +190,7 @@ with tab1:
         if st.button("⬅️ Volver al Radar"):
             st.session_state.view = 'list'
             st.rerun()
-        return
+            st.stop()
             
         # PROCESAMIENTO CON MOTOR
         if sid not in st.session_state.senales_cache:
@@ -279,7 +278,6 @@ with tab2:
                     st.session_state.selected_news = (noti, next((a for a in activos_db if a["symbol"] in str(noti.get("related_assets"))), activos_db[0]), f"sig_{noti['id']}")
                     st.session_state.view = 'detail'
                     st.switch_page("app.py")
-                    return
 
 with tab3:
     st.subheader("📑 Reporte de Compliance")
